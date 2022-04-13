d72c9e86de92c6d3b38ddcaa0a11c6f1e7577e83
xunhuangxxx <xunhuangxxx@gmail.com>
2022-04-13 01:28:23 -0400

    all function done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 
+//create a function to insert Html to parentelement
+function insertInfo (position, content){
+  position.insertAdjacentHTML('beforeend', content);
+}
 /*
 Create the `showPage` function
 */
@@ -19,31 +23,52 @@ function showPage (list, page){
       const studentImg = `<img class="avatar" src=${list[i].picture.thumbnail} alt="Profile Picture">`;
       const studentName = `<h3>${list[i].name.first} ${list[i].name.last}</h3>`;
       const studentEmail = `<span class="email">${list[i].email}</span>`;
-
-      div.insertAdjacentHTML('beforeend', studentImg);
-      div.insertAdjacentHTML('beforeend', studentName);
-      div.insertAdjacentHTML('beforeend', studentEmail);
-
-
       const studentDate =
         `<div class="joined-details">
           <span class="date">Joined ${list[i].registered.date}</span>
         </div>`;
 
-      li.insertAdjacentHTML('beforeend', studentDate);
+     insertInfo(div, studentImg);
+     insertInfo(div, studentName);
+     insertInfo(div, studentEmail);
+     insertInfo(div, studentDate);
 
-
-      studentList.appendChild(li);
+     studentList.appendChild(li);
     }
   }
 }
-
-
 /*
 Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
 */
+function addPagination (list) {
+  const totalButtons = Math.ceil(list.length / 9);
+  const linkList = document.querySelector('.link-list');
+  linkList.innerHTML = '';
+//create pagination buttons
+  for (let i = 1; i <= totalButtons; i++){
+    const li = document.createElement('li');
+    button = `<button type="button">${i}</button>`;
+    insertInfo(li, button);
+    linkList.appendChild(li);
+  }
+  linkList.querySelector('button').className = 'active';
 
+//create event listener for buttons
+  linkList.addEventListener('click', (e) => {
+    if(e.target.tagName === 'BUTTON') {
+      const buttonLists = linkList.querySelectorAll('button');
+      for(let i = 0; i < buttonLists.length; i++){
+        buttonLists[i].className = '';
+      }
+      e.target.className = 'active';
+    }
+//Call showPage function
+    const page = e.target.textContent;
+    showPage(list, page);
+  });
+}
 
 
 // Call functions
+showPage(data, 1);
+addPagination(data);
